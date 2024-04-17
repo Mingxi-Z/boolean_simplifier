83406b4b4c5d9042cf977dd336e579420cbd31d5
mingxi <mingxiz@hotmail.com>
2024-04-17 09:50:03 +0800

    initialize project

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Quine-McCluskey/src/logical_expr.hpp"
+#include "Quine-McCluskey/src/quine_mccluskey.hpp"
+
+int main(int, char**){
+    std::cout << "Hello, from boolean_simplify!\n";
+}
