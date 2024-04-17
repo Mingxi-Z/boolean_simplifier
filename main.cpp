ae660cee8c0e0f05b7ef3c0ddcf40755cb040643
mingxi <mingxiz@hotmail.com>
2024-04-17 15:09:17 +0800

    change mcdc and minbool to submodule

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include "MCDC/mcdc/Include/mcdc.hpp"
 
 int main(int, char**){
-    string s = "a&b&c";
+    string s = "a & b & c";
     MintermCalculator c(s);
     MintermVector v = c.calculate();
     for (int i : v) {
